a4571c94ab0da24feef5d0f1515e5b57e35f6e13
Madalina-Nicolescu <62146366+Madalina-Nicolescu@users.noreply.github.com>
2020-05-13 20:57:49 +0300

    first commit

---
+++ Dictionar1/KeyComp.h
@@ -0,0 +1,38 @@
+#pragma once
+#include<string>
+using namespace std;
+
+template <class K> class KeyComp
+{
+
+public:
+	int operator()(const K& k1, const K& k2) { 
+		if (k1 < k2)
+			return -1;
+		else if (k1 == k2)
+			return 0;
+		return 1;
+	}
+};
+
+template<> class KeyComp<string>
+{
+public:
+	int operator()(const string& k1, const string& k2)
+	{
+		int n;
+		if (k1.length() > k2.length())
+			n = k2.length() / 2;
+		else n = k1.length() / 2;
+
+		if (k1.substr(0, n) == k2.substr(0, n))
+			return 0;
+		else
+		{
+			for (int i = 0; i <= n; i++)
+					if (k1[i] < k2[i])
+						return -1;
+		}
+		return 1;
+	}
+};
